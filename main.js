86ff3f49fec6ed2ac9022396e1ec66796f1c488d
clarisscheonggg <clariss.cheongkw@gmail.com>
2026-07-25 19:16:30 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,134 @@
+/*declaration*/
+const page1btn = document.querySelector("#page1btn");
+const page2btn = document.querySelector("#page2btn");
+const page3btn = document.querySelector("#page3btn");
+var allpages = document.querySelectorAll(".page");
+
+//hide the subtopic content
+function hideall() {
+    for (let onepage of allpages) {
+        onepage.style.display = "none"; //hide page
+    }
+}
+
+// Function to show selected page
+let currentPage = 0;
+
+function show(pgno) {
+
+    // If the page is already open, close it
+    if (currentPage === pgno) {
+        hideall();
+        currentPage = 0;
+        return;
+    }
+
+    // Otherwise show the selected page
+    hideall();
+    let onepage = document.querySelector("#page" + pgno);
+    onepage.style.display = "block";
+    currentPage = pgno;
+}
+
+//open the content
+const menu = document.querySelector("#menu");
+
+menu.addEventListener("click", function(event) {
+
+    if(event.target.id == "page1btn"){
+        show(1);
+    }
+
+    else if(event.target.id == "page2btn"){
+        show(2);
+    }
+
+    else if(event.target.id == "page3btn"){
+        show(3);
+    }
+
+});
+
+// Hide all pages when the website first loads
+hideall();
+
+//c note audio 
+const Caudio = document.getElementById("CnoteAudio");
+const CnoteBtn = document.getElementById("CnoteBtn");
+
+CnoteBtn.addEventListener("click", function () {
+
+    CnoteBtn.classList.add("playing");
+
+    Caudio.currentTime = 0;
+    Caudio.play();
+
+    setTimeout(function () {
+        CnoteBtn.classList.remove("playing");
+    }, 500);
+
+});
+//G note
+const Gaudio = document.getElementById("GnoteAudio");
+const GnoteBtn = document.getElementById("GnoteBtn");
+
+GnoteBtn.addEventListener("click", function () {
+
+    GnoteBtn.classList.add("playing");
+
+    Gaudio.currentTime = 0;
+    Gaudio.play();
+
+    setTimeout(function () {
+        GnoteBtn.classList.remove("playing");
+    }, 500);
+
+});
+
+//F note
+const Faudio = document.getElementById("FnoteAudio");
+const FnoteBtn = document.getElementById("FnoteBtn");
+
+FnoteBtn.addEventListener("click", function () {
+
+    FnoteBtn.classList.add("playing");
+
+    Faudio.currentTime = 0;
+    Faudio.play();
+
+    setTimeout(function () {
+        FnoteBtn.classList.remove("playing");
+    }, 500);
+
+});
+//quiz
+const btnSubmit=document.querySelector("#btnSubmit");
+btnSubmit.addEventListener("click",CheckAns);
+const scorebox=document.querySelector("#scorebox");
+var q1,q2,q3,score=0;
+function CheckAns(){
+score=0; //reset score to 0, check ans and give score if correct
+//read the value of the selected radio button for q1
+q1=document.querySelector("input[name='q1']:checked").value;
+console.log(q1); //check q1 value retrieved
+if(q1=="F")score++;
+//read the value of the selected radio button for q2
+q2=document.querySelector("input[name='q2']:checked").value;
+console.log(q2); //check q2 value retrieved
+if(q2=="G")score++;
+q3=document.querySelector("input[name='q3']:checked").value;
+console.log(q3); //check q2 value retrieved
+if(q3=="C")score++;
+scorebox.innerHTML="Score: "+score;
+/*time out*/
+setTimeout(function(){
+    scorebox.innerHTML += "<br>Thanks for playing!";
+},1000);
+}
+
+// Hamburger menu
+const hamIcon = document.querySelector("#hamIcon");
+
+hamIcon.addEventListener("click", function () {
+    menu.classList.toggle("showMenu");
+});
